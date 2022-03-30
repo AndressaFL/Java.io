6bacc2511859259f41367b612d323b8cdb1a285e
AndressaFL <100968181+AndressaFL@users.noreply.github.com>
2022-03-29 22:19:12 -0500

    Update file

--- Cliente.java
+++ Cliente.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * Classe que representa um cliente no Bytebank.
  *
- * @author Nico Steppat
+ * @author Andressa Lazoti
  * @version 0.1
  */
 public class Cliente implements Serializable {
